bcf58927ce71ffdea0c4114e9a34c8aaa993a994
kaufmanisaac19 <kaufman.isaac19@gmail.com>
2026-04-09 17:09:54 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE='podgen-v5';
-const CORE=['/','/index.html','/manifest.json','/icons/icon-192.png','/icons/icon-512.png'];
+const CORE=['/PodGen/','/PodGen/index.html','/PodGen/manifest.json','/PodGen/icons/icon-192.png','/PodGen/icons/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{
